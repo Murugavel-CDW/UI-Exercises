c40dd99f1fa381df954af5ea130aca8ac3f362b0
Murugavel-CDW <murugavel.v@cdw.com>
2025-04-10 10:19:46 +0530

    fix: prompt input from user

--- calculator.js
+++ calculator.js
@@ -1,62 +1,57 @@
 //  Function to check if both the passed values are numbers
 const checkIfBothAreNumber = (num1, num2) => {
-    return typeof (num1) == "number" && typeof (num2) == "number";
+    if (!(typeof (num1) == "number" && typeof (num2) == "number")) {
+        alertMessage("Enter valid number input");
+    }
+}
+
+const alertMessage = (message) => {
+    window.alert(message);
 }
 
 // Calculator object
 const calculator = {
     // Add Functionality
     add: (num1, num2) => {
-        if (!checkIfBothAreNumber(num1, num2)) {
-            return 0;
-        }
-        return num1 + num2;
+        checkIfBothAreNumber(num1, num2);
+        alertMessage(`${num1} + ${num2} = ${num1 + num2}`);
     },
     // Subtract Functionality
     subtract: (num1, num2) => {
-        if (!checkIfBothAreNumber(num1, num2)) {
-            return 0;
-        }
-        return num1 - num2
+        checkIfBothAreNumber(num1, num2);
+        alertMessage(`${num1} - ${num2} = ${num1 - num2}`);
     },
     // Multiply Functionality
     multiply: (num1, num2) => {
-        if (!checkIfBothAreNumber(num1, num2)) {
-            return 0;
-        }
-        return num1 * num2;
+        checkIfBothAreNumber(num1, num2);
+        alertMessage(`${num1} * ${num2} = ${num1 * num2}`);
     },
     // Divide Functionality
     divide: (num1, num2) => {
-        if (!checkIfBothAreNumber(num1, num2) || num2 === 0) {
-            return 0;
-        }
-        return num1 / num2;
+        checkIfBothAreNumber(num1, num2);
+        if(num2 == 0) alertMessage("num2 cannot be zero")
+        alertMessage(`${num1} / ${num2} = ${num1 / num2}`);
     }
 }
 
-
-
-
-console.log(`1 + 2 = ${calculator.add(1, 2)}`);
-console.log(calculator.add("apple", 1)); // logs as 0
-console.log(`5 - 3 = ${calculator.subtract(5, 3)}`);
-console.log(`5 * 5 = ${calculator.multiply(5, 5)}`);
-console.log(`5 / 0 = ${calculator.divide(5, 0)}`);
-console.log(calculator.divide("apple", 0));
-console.log(`25 / 5 = ${calculator.divide(25, 5)}`);
-
-
-let a = {
-    name: "nike",
-    age: {
-        num: 20
-    }
+const num1 = window.prompt("Enter value of num1");
+const num2 = window.prompt("Enter value of num2");
+const operation = window.prompt("Enter the operation").trim().toLowerCase();
+if (operation) {
+    calculator[operation](parseFloat(num1), parseFloat(num2));
 }
 
-let b = { ...a }
-b.age.num = 50;
 
-console.log(a.age.num);
-console.log(b.age.num);
+// let a = {
+//     name: "nike",
+//     age: {
+//         num: 20
+//     }
+// }
+
+// let b = { ...a }
+// b.age.num = 50;
+
+// console.log(a.age.num);
+// console.log(b.age.num);
 
