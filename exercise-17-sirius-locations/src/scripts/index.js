7da5ef1f122c02a6da956f5460d554579060192e
Murugavel-CDW <murugavel.v@cdw.com>
2025-04-11 23:29:24 +0530

    feat: completion of the exercise

--- exercise-17-sirius-locations/src/scripts/index.js
+++ exercise-17-sirius-locations/src/scripts/index.js
@@ -1,3 +1,25 @@
-$( function() {
-    $( "#tabs" ).tabs();
-  } );
+import { locationsData } from "../assets/data/locations.js";
+
+// Fragment node to avoid dom manipulation at every step
+const fragmentNode = document.createDocumentFragment();
+
+// Iterating over the locations data array
+for (const locationData of locationsData) {
+  const rowElement = $("<div></div>").addClass("locations-info-row"); // container element for a row
+  const imageContainer = $("<div></div>")
+    .addClass("country-image-container")
+    .html(`<img src="../src/assets/images/flag.png" alt="Country flag" class="location-country">`);
+  const stateTextElement = $("<p></p>").addClass("location-info-text").text(locationData.state);
+  const cityTextElement = $("<p></p>").addClass("location-info-text").text(locationData.city);
+  const contactTextElement = $("<p></p>").addClass("location-info-text").text(locationData.contact);
+  // appending elements to the row container
+  rowElement.append(imageContainer, stateTextElement, cityTextElement, contactTextElement);
+  fragmentNode.appendChild(rowElement[0]); // appending to the fragment node
+}
+
+$("#locations").append(fragmentNode);
+
+$(function () {
+  $("#solutions").accordion(); // enabling the accordion functionality
+  $("#tabs").tabs();
+});
