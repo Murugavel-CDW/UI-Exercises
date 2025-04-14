c5a37726ee5bc27e9fa2f5ec47eb472c60ea5991
Murugavel-CDW <murugavel.v@cdw.com>
2025-04-14 13:26:30 +0530

    feat: dynamic addition of flags in script file

--- exercise-17-sirius-locations/src/scripts/index.js
+++ exercise-17-sirius-locations/src/scripts/index.js
@@ -5,13 +5,14 @@ const fragmentNode = document.createDocumentFragment();
 
 // Iterating over the locations data array
 for (const locationData of locationsData) {
-  const rowElement = $("<div></div>").addClass("locations-info-row"); // container element for a row
-  const imageContainer = $("<div></div>")
+  const countryName = locationData.country.replaceAll(" ", "-").toLowerCase();
+  const rowElement = $("<div>").addClass("locations-info-row"); // container element for a row
+  const imageContainer = $("<div>")
     .addClass("country-image-container")
-    .html(`<img src="../src/assets/images/flag.png" alt="Country flag" class="location-country">`);
-  const stateTextElement = $("<p></p>").addClass("location-info-text").text(locationData.state);
-  const cityTextElement = $("<p></p>").addClass("location-info-text").text(locationData.city);
-  const contactTextElement = $("<p></p>").addClass("location-info-text").text(locationData.contact);
+    .html(`<img src="../src/assets/images/flags/${countryName}.png" alt="Country flag" class="location-country">`);
+  const stateTextElement = $("<p>").addClass("location-info-text").text(locationData.state);
+  const cityTextElement = $("<p>").addClass("location-info-text").text(locationData.city);
+  const contactTextElement = $("<p>").addClass("location-info-text").text(locationData.contact);
   // appending elements to the row container
   rowElement.append(imageContainer, stateTextElement, cityTextElement, contactTextElement);
   fragmentNode.appendChild(rowElement[0]); // appending to the fragment node
