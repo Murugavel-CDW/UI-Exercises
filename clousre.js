86e704bbbd84f21c0cf239aeee17d34f75c514e9
Murugavel-CDW <murugavel.v@cdw.com>
2025-04-21 21:36:32 +0530

    fix: fixing of the exposed methods

--- clousre.js
+++ clousre.js
@@ -1,92 +1,98 @@
 const checkValidNumber = amount => amount && typeof (amount) == "number"; 
 
-const createAccountClosure = () => {
-    let accountBalance = 0;
+const bankInstance = (() => {
+    // Customer account list
+    const bankUserList = [
+        {
+            accountNumber: 123456,
+            cardNumber: 1234567890,
+            pin: 2468,
+            balance: 0
+        },
+        {
+            accountNumber: 678901,
+            cardNumber: 2345678901,
+            pin: 8956,
+            balance: 0
+        },
+        {
+            accountNumber: 1112134,
+            cardNumber: 3456789012,
+            pin: 2875,
+            balance: 0
+        },
+        {
+            accountNumber: 2875490,
+            cardNumber: 8217452099,
+            pin: 3287,
+            balance: 0
+        },
+        {
+            accountNumber: 918237,
+            cardNumber: 2817364223,
+            pin: 9028,
+            balance: 0
+        }
+    ];
+    
+    // Log into account
+    const login = () => {
+        const accountNumber = parseInt(window.prompt("Enter account number"));
+        if (!checkValidNumber(accountNumber)) {
+            return window.alert("Enter a valid number"); // if account number is not valid
+        }
+        const customerObj = bankUserList.find(customer => customer.accountNumber === accountNumber); // fetching the customer with the accountNumber
+        return customerObj;
+    };
 
     // Withdraw function
-    const withdrawAmount = (pin, cardNumber) => {
+    const withdrawAmount = () => {
+        const customerObj = login();
+        if (!customerObj) {
+            return window.alert("User with account number doesnt exist");
+        } 
         const enteredCardNumber = parseInt(window.prompt("Enter card number"));
         const enteredPin = parseInt(window.prompt("Enter PIN"));
-        if (cardNumber != enteredCardNumber || pin != enteredPin) {
+        if (customerObj.cardNumber != enteredCardNumber || customerObj.pin != enteredPin) {
             window.alert("Card Number / Pin does not match");
             return;
         }
         const amount = parseFloat(window.prompt("Enter the amount to withdraw"));
-        if (!checkValidNumber(amount) || accountBalance < 0) {
+        if (!checkValidNumber(amount) || customerObj.balance < 0) {
             window.alert("Enter a valid amount");
         } else {
-            accountBalance -= amount;
-            window.alert(`Amount withdrawn successfully.\n Withdrawn amount: ${amount}; Current Account Balance: ${accountBalance}`);
+            customerObj.balance -= amount;
+            window.alert(`Amount withdrawn successfully.\n Withdrawn amount: ${amount}; Current Account Balance: ${customerObj.balance}`);
         }
     };
 
     // Deposit function
-    const depositAmount = (pin, cardNumber) => {
+    const depositAmount = () => {
+        const customerObj = login();
+        if (!customerObj) {
+            return window.alert("User with account number doesnt exist");
+        } 
         const enteredCardNumber = parseInt(window.prompt("Enter card number"));
         const enteredPin = parseInt(window.prompt("Enter PIN"));
-        if (cardNumber != enteredCardNumber || pin != enteredPin) {
+        if (customerObj.cardNumber != enteredCardNumber || customerObj.pin != enteredPin) {
             window.alert("Card Number / Pin does not match");
             return;
         }
         const amount = parseFloat(window.prompt("Enter the amount to deposit"));
         if (!checkValidNumber(amount)) {
             window.alert("Enter a valid amount");
-        }else {
-            accountBalance += amount;
-            window.alert(`Amount Deposited successfully.\n Deposited amount: ${amount}; Current Account Balance: ${accountBalance}`);
+        } else {
+            customerObj.balance += amount;
+            window.alert(`Amount Deposited successfully.\n Deposited amount: ${amount}; Current Account Balance: ${customerObj.balance}`);
         }
     }
     return { withdrawAmount, depositAmount };
-}
-
-// Customer account list
-const bankAccounts = [
-    {
-        accountNumber: 123456,
-        cardNumber: 1234567890,
-        pin: 2468,
-        accountOperations: createAccountClosure()
-    },
-    {
-        accountNumber: 678901,
-        cardNumber: 2345678901,
-        pin: 8956,
-        accountOperations: createAccountClosure()
-    },
-    {
-        accountNumber: 1112134,
-        cardNumber: 3456789012,
-        pin: 2875,
-        accountOperations: createAccountClosure()
-    },
-    {
-        accountNumber: 2875490,
-        cardNumber: 8217452099,
-        pin: 3287,
-        accountOperations: createAccountClosure()
-    },
-    {
-        accountNumber: 918237,
-        cardNumber: 2817364223,
-        pin: 9028,
-        accountOperations: createAccountClosure()
-    },
-];
+})();
 
 const performOperations = () => {
-    const accountNumber = parseInt(window.prompt("Enter account number"));
-    if (!checkValidNumber(accountNumber)) {
-        return window.alert("Enter a valid number"); // if account number is not valid
-    } 
-    const customerObj = bankAccounts.find(customer => customer.accountNumber === accountNumber); // fetching the customer with the accountNumber
-    if (!customerObj) {
-        return window.alert("User with account number doesnt exist");
-    } 
-    const pinNumber = customerObj.pin;
-    const cardNumber = customerObj.cardNumber;
-    customerObj.accountOperations.depositAmount(pinNumber, cardNumber);
-    customerObj.accountOperations.depositAmount(pinNumber, cardNumber);
-    customerObj.accountOperations.withdrawAmount(pinNumber, cardNumber);
+    bankInstance.depositAmount();
+    bankInstance.depositAmount();
+    bankInstance.withdrawAmount();
 }
 
 performOperations();
